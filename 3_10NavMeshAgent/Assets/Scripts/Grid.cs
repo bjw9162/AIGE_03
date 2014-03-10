79a26f5f30894a1eb048e7ab9b7dbd8f6aa69b03
Gabrielle Bennett <gmb9280@rit.edu>
2014-03-10 08:43:24 -0400

    Nav Meshes implemented. Bugged out werewolves and villagers.

---
+++ 3_10NavMeshAgent/Assets/Scripts/Grid.cs
@@ -0,0 +1,53 @@
+/*using UnityEngine;
+using System.Collections;
+//including some .NET for dynamic arrays called List in C#
+using System.Collections.Generic;
+
+[System.Serializable]
+
+This class will be used alongside Quadrant
+to make the map into a searchable datastructure 
+
+public class Grid
+{
+	// Attributes and such
+	float width, height; // of the total Grid
+	float nRows, nCols; // Determined by the size
+	float wRow, hRow;
+
+	private ArrayList<Quadrant> quads;
+
+	//Each grid is made up of Quadrants. Think rows, columns, and cells.
+
+	// Use this for initialization. Rows and columns created using width and height
+	void Start (float w_, float h_) {
+
+		quads = new ArrayList<Quadrant>(); // instantiate new ArrayList of quads
+
+		Debug.Log("Grid created.");
+
+	}
+
+	void InstantiateGrid()
+	{
+		// for now, let's just make it 10x10 to make things easy
+		for(float i = 0; i < 10; i++)
+		{
+			// rows
+			for(float j = 0; j < 10; j++)
+			{
+				// columns. eg, (i,j)
+				//quads.Add(new Quadrant(
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}*/
+
---
+++ 3_10NavMeshAgent/Assets/Scripts/NavAgent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavAgent : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+		agent = GetComponent<NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Basically just set this to a Vec3
+		//agent.SetDestination(targetPoint);
+	}
+}
